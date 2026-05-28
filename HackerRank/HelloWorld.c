28fdaea669f301e4f7fbe8fcd7a5bc900f10e0e2
supanxt <non.supanatsukprasert@gmail.com>
2026-05-28 20:43:44 +0700

    Do the same thing but not have output

--- HackerRank/HelloWorld.c
+++ HackerRank/HelloWorld.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-int main(){
-    
-    char s[100];
 
-    scanf("%[^\n]%*c", s);
+int main() {
+
+    // string sentence;         // There is no data type of string in C :(
+    char sentence[100];            // But there is `char` data type with `Array` in C :)
+    // and you have to specify the size of Array...
+
+    // scanf() can be used to take `string` input but should not used the ampersand `&` to addressing data 
+    // Because `sentence` is an array so itself is an addresse, if we addresse just one indax we have to do. 
+    // scanf("%s", sentence);     // The scanf("%s") take only one word before first whitespace :(
+    // So I will use a new mathod learned from Hacker Rank...
+    scanf("%[^\n]%*c", sentence);
+    // `%[]` it called Scanset, scanf read only inside of this.
+    // `^\n` means : Take inputs, untill a new line appears...
+    // `%*c` means : Delete buffer from the end of line made by Enter. 
+
     printf("Hello, World!\n");
-    printf("%s\n",s);
+    printf("%s\n", sentence);
 
     return 0;
 }
